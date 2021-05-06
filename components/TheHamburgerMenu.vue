c4f0586e5317b3a14dee7cd7b62a955c7f4b6313
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-05-06 21:31:00 +0200

    rh3h5h

--- components/TheHamburgerMenu.vue
+++ components/TheHamburgerMenu.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav class="bg-apple-green fixed inset-0 z-50">
+  <nav class="bg-electric-pink fixed inset-0 z-50">
     <div class="container mx-auto pt-[20px] px-[18px] grid grid-cols-3">
       <div class="col-start-2">
-        <img class="mx-auto" src="~/assets/images/logo-pink.svg" />
+        <img class="mx-auto" src="~/assets/images/logo-green.svg" />
       </div>
       <div class="flex justify-end">
         <button @click="toggleMenu">
@@ -10,6 +10,45 @@
         </button>
       </div>
     </div>
+    <ul
+      class="absolute bottom-[60px] inset-x-0 px-[19px] h-[380px] flex flex-col justify-between"
+    >
+      <li>
+        <NuxtLink
+          class="font-serif font-light text-apple-green text-[60px] leading-[40px]"
+          to="/about"
+          >About me</NuxtLink
+        >
+      </li>
+      <li>
+        <NuxtLink
+          class="font-serif font-light text-apple-green text-[60px] leading-[40px]"
+          to="/portfolio"
+          >Portfolio</NuxtLink
+        >
+      </li>
+      <li>
+        <NuxtLink
+          class="font-serif font-light text-apple-green text-[60px] leading-[40px]"
+          to="/press"
+          >Press</NuxtLink
+        >
+      </li>
+      <li>
+        <NuxtLink
+          class="font-serif font-light text-apple-green text-[60px] leading-[40px]"
+          to="/news"
+          >News</NuxtLink
+        >
+      </li>
+      <li>
+        <NuxtLink
+          class="font-serif font-light text-apple-green text-[60px] leading-[40px]"
+          to="/contact"
+          >Contact</NuxtLink
+        >
+      </li>
+    </ul>
   </nav>
 </template>
 
