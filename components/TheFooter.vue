38463db6412c4ebecd306ae177aed3784d872221
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-05-06 17:04:52 +0200

    first commit

---
+++ components/TheFooter.vue
@@ -0,0 +1,95 @@
+<template>
+  <footer class="bg-electric-pink">
+    <div class="container mx-auto pt-[50px] pb-[10px] px-[17.5px]">
+      <div class="flex flex-col items-center">
+        <div>
+          <img src="~assets/images/logo.svg" />
+        </div>
+        <div class="mt-[50px]">
+          <div>
+            <div
+              class="font-serif font-light text-[24px] leading-[35px] text-center"
+            >
+              Want to get in touch?
+            </div>
+            <a
+              class="block font-light leading-[32px] text-center"
+              href="+ 31 (0)10 234 5413"
+              >+ 31 (0)10 234 5413</a
+            >
+            <a
+              class="block font-light leading-[32px] text-center"
+              href="info@lolavanpraag.com"
+              >info@lolavanpraag.com</a
+            >
+          </div>
+
+          <div class="mt-[40px]">
+            <div
+              class="font-serif font-light text-[24px] leading-[35px] text-center"
+            >
+              Find my work at the follow galleries
+            </div>
+            <a
+              class="block font-light leading-[32px] text-center"
+              href="https://www.rademakersgallery.com/"
+            >
+              Rademarkers Gallery
+            </a>
+            <a
+              class="block font-light leading-[32px] text-center"
+              href="https://hetechtewerk.studio/"
+              >HEW Gallery</a
+            >
+          </div>
+
+          <div class="mt-[40px]">
+            <div
+              class="font-serif font-light text-[24px] leading-[35px] text-center"
+            >
+              Follow Lola on
+            </div>
+            <a
+              class="block font-light leading-[32px] text-center"
+              href="https://www.instagram.com/lolavanpraag/"
+            >
+              Instagram
+            </a>
+            <a
+              class="block font-light leading-[32px] text-center"
+              href="https://www.linkedin.com/in/lola-van-praag/"
+              >LinkedIn</a
+            >
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="mt-[50px] pt-[50px] border-t border-[#F941CB] flex flex-col items-center"
+      >
+        <div class="w-full">
+          <div
+            class="font-serif font-light text-[24px] leading-[35px] text-center"
+          >
+            Join my mailinglist
+          </div>
+          <div
+            class="mt-[20px] border-b border-black flex justify-between items-baseline"
+          >
+            <input
+              class="pb-[17px] pl-[17px] bg-electric-pink font-light text-[14px] leading-[19px] placeholder-black"
+              type="text"
+              placeholder="Enter your mail"
+            />
+            <button class="h-full">
+              <img src="~/assets/images/arrow.svg" />
+            </button>
+          </div>
+        </div>
+        <div class="mt-[50px] font-light text-[12px] leading-[17px]">
+          &copy; 2021 Lola van Praag
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px"
+    },
+    fontFamily: {
+      sans: ["halyard-display", "sans-serif"],
+      serif: ["ivypresto-display", "serif"]
+    },
+    extend: {
+      colors: {
+        "electric-pink": "#FF67D9"
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
