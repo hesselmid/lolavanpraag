dcbc402bf478df53f313c1e962231b5d8c0648a8
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-05-09 16:43:22 +0200

    qiwiqwiucw

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,17 @@
     <div id="hero">
       <div class="bg-dusty-orange h-full bg-opacity-90">
         <div
-          class="container mx-auto pt-[191px] pb-[48px] px-[17.5px] md:px-[30px] 2xl:pt-[266px] 2xl:pb-[53px] 2xl:px-[55px]"
+          class="container mx-auto pt-[191px] pb-[48px] px-[17.5px] md:px-[30px] xl:pt-[266px] xl:pb-[53px] xl:px-[55px]"
         >
           <p
-            class="font-serif text-electric-pink text-[34px] leading-[40px] 2xl:text-[84px] 2xl:leading-[86px]"
+            class="font-serif text-electric-pink text-[34px] leading-[40px] xl:text-[84px] xl:leading-[86px]"
           >
             Colour was swapped out for sequins in Lola van Praag’s
             <span class="font-sans underline">NO UNLOCALS</span>
             graduate collection. She sent out brightly sparkling models dressed
             in tight-fitting sequin-heavy wetsuits.
           </p>
-          <div class="mt-[40px] ml-[20px] 2xl:mt-[60px] 2xl:ml-[30px]">
+          <div class="mt-[40px] ml-[20px] xl:mt-[60px] xl:ml-[30px]">
             <img class="inline-block" src="~/assets/images/arrow-pink.svg" />
             <div
               class="inline-block ml-[29px] lowercase text-[18px] leading-[24px] text-electric-pink"
@@ -21,7 +21,7 @@
               View project
             </div>
           </div>
-          <div class="hidden mt-[54px] 2xl:block">
+          <div class="hidden mt-[54px] xl:block">
             <img class="mx-auto" src="~/assets/images/scroll-icon.svg" />
           </div>
         </div>
