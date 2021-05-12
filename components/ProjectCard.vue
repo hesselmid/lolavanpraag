53af216885efa2ce84c1a300748d0d590eeb3b3c
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-05-12 14:25:10 +0200

    qoqwiviehov

---
+++ components/ProjectCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <li class="mt-[40px] max-w-[340px] mx-auto">
+    <img :src="img" />
+    <div class="pt-[10px]">
+      <div class="flex justify-between">
+        <h2 class="font-light text-[22px] leading-[31px]">{{ title }}</h2>
+        <div
+          class="inline-block mt-[3px] py-[3px] px-[10px] border rounded-[13px] border-electric-pink font-light text-[12px] leading-[17px] text-electric-pink lowercase"
+        >
+          {{ category }}
+        </div>
+      </div>
+      <p class="mt-[7px] w-[250px] font-light text-[14px] leading-[19px]">
+        {{ description }}
+      </p>
+      <div class="mt-[15px] ml-[20px] group">
+        <div>
+          <img
+            class="inline-block w-[18px] group-hover:hidden"
+            src="~assets/images/arrow.svg"
+          />
+          <img
+            class="hidden w-[18px] group-hover:inline-block"
+            src="~assets/images/arrow-pink.svg"
+          />
+
+          <div
+            class="ml-[29px] inline-block leading-[22px] lowercase group-hover:text-electric-pink group-hover:underline"
+          >
+            View project
+          </div>
+        </div>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["title", "description", "category", "img", "slug"]
+};
+</script>
+
+<style scoped>
+li:first-of-type {
+  margin-top: 0;
+}
+</style>
---
+++ pages/portfolio.vue
@@ -0,0 +1,70 @@
+<template>
+  <div
+    class="container mx-auto pt-[86px] pb-[100px] px-[17.5px] md:px-[30px] 2xl:px-[55px]"
+  >
+    <Breadcrumbs :breadcrumbs="breadcrumbs" color="#A3A3A3" />
+    <h1 class="mt-[20px] font-serif font-light text-[32px] leading-[32px]">
+      Portfolio
+    </h1>
+    <ul class="mt-[20px]">
+      <ProjectCard
+        v-for="project in projects"
+        :key="project.slug"
+        :title="project.title"
+        :description="project.description"
+        :category="project.category"
+        :img="project.img"
+        :slug="project.slug"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      breadcrumbs: [
+        { name: "Home", link: "/" },
+        { name: "Portfolio", link: "/portfolio" }
+      ],
+      projects: [
+        {
+          title: "No Unlocals",
+          description:
+            "Graduation Collection 2019 – MA Menswear, Westminster University",
+          category: "mode",
+          img: require(`~/assets/images/project-thumbnail-1.jpg`),
+          slug: "/"
+        },
+        {
+          title: "It’s Not a Flower",
+          description:
+            "Graduation Collection - MA Textile, Swedish School of Textiles",
+          category: "textile",
+          img: require(`~/assets/images/project-thumbnail-2.jpg`),
+          slug: "/"
+        },
+        {
+          title: "Floral Lines",
+          description:
+            "Graduation Collection 2014 - BA Fashion, Willem de Kooning Academie",
+          category: "mode",
+          img: require(`~/assets/images/project-thumbnail-3.jpg`),
+          slug: "/"
+        }
+      ]
+    };
+  },
+  head: {
+    title: "Portfolio | lolavanpraag.com",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content: ""
+      }
+    ]
+  }
+};
+</script>
