26b0ca36f9bbca57a8b633ecd9a8b7697df8585e
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-05-14 22:49:31 +0200

    efo3ig35h

--- pages/portfolio/no-unlocals.vue
+++ pages/portfolio/no-unlocals.vue
@@ -1,40 +1,47 @@
 <template>
   <div
-    class="container mx-auto pt-[86px] pb-[100px] px-[17.5px] md:px-[30px] 2xl:px-[55px]"
+    class="container mx-auto pt-[86px] pb-[100px] px-[17.5px] md:px-[30px] 2xl:pt-[165px] 2xl:pb-[228px] 2xl:px-[55px]"
   >
     <Breadcrumbs :breadcrumbs="breadcrumbs" color="#A3A3A3" />
-    <h1 class="mt-[20px] font-serif font-light text-[32px] leading-[32px]">
+    <h1
+      class="mt-[20px] font-serif font-light text-[32px] leading-[32px] 2xl:mt-[15px] 2xl:text-[90px] 2xl:leading-[90px]"
+    >
       No Unlocals
     </h1>
-    <p class="mt-[10px] font-light leading-[22px]">
-      My graduate collection, NO UNLOCALS, is inspired by the 70’s and 80’s surf
-      scene in California. The work of iconic surf photographers Jeff Divine and
-      Hugh Holland served as a perfect departure point. Their epic photographs
-      provide a vivid and colourful testimony of the evolution of the culture
-      and fashion in the surf scene in that time and place
-    </p>
-    <h2 class="mt-[20px] font-medium leading-[22px]">Materials used:</h2>
-    <ul>
-      <li class="font-light leading-[22px]">Neoprene</li>
-      <li class="font-light leading-[22px]">Cotton</li>
-    </ul>
-    <h2 class="mt-[20px] font-medium leading-[22px]">
-      Find this collection at the following gallery:
-    </h2>
-    <a
-      class="leading-[22px] underline"
-      href="https://www.rademakersgallery.com/"
-      target="_blank"
-      rel="nofollow noreferrer noopener"
-    >
-      Rademarkers Gallery
-    </a>
-    <div class="mt-[30px]">
-      <NuxtPicture src="/images/no-unlocals-1.jpg" />
+    <div>
+      <p
+        class="mt-[10px] font-light leading-[22px] 2xl:mt-[15px] 2xl:text-[24px] 2xl:leading-[30px]"
+      >
+        My graduate collection, NO UNLOCALS, is inspired by the 70’s and 80’s
+        surf scene in California. The work of iconic surf photographers Jeff
+        Divine and Hugh Holland served as a perfect departure point. Their epic
+        photographs provide a vivid and colourful testimony of the evolution of
+        the culture and fashion in the surf scene in that time and place
+      </p>
+      <h2 class="mt-[20px] font-medium leading-[22px]">Materials used:</h2>
+      <ul>
+        <li class="font-light leading-[22px]">Neoprene</li>
+        <li class="font-light leading-[22px]">Cotton</li>
+      </ul>
+      <h2 class="mt-[20px] font-medium leading-[22px]">
+        Find this collection at the following gallery:
+      </h2>
+      <a
+        class="leading-[22px] underline"
+        href="https://www.rademakersgallery.com/"
+        target="_blank"
+        rel="nofollow noreferrer noopener"
+      >
+        Rademarkers Gallery
+      </a>
+      <div class="mt-[30px]">
+        <NuxtPicture src="/images/no-unlocals-1.jpg" />
+      </div>
+      <div class="mt-[15px]">
+        <NuxtPicture src="/images/no-unlocals-2.jpg" />
+      </div>
     </div>
-    <div class="mt-[15px]">
-      <NuxtPicture src="/images/no-unlocals-2.jpg" />
-    </div>
+    <Marquee class="hidden 2xl:block" />
     <p class="mt-[30px] font-light leading-[22px]">
       To kick-start this project I collected a bunch of old wetsuits. I
       deconstructed these in order to learn about the way they are built and to
