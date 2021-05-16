4cb091dbf634c580841acfd8075ffb470414f8e6
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-05-16 13:07:16 +0200

    kogu4g

--- pages/about.vue
+++ pages/about.vue
@@ -1,21 +1,48 @@
 <template>
   <div>
     <div
-      class="container mx-auto pt-[86px] px-[17.5px] md:px-[30px] md:pt-[165px] 2xl:px-[55px]"
+      class="
+        container
+        mx-auto
+        pt-[86px]
+        px-[17.5px]
+        md:px-[30px]
+        md:pt-[165px]
+        2xl:px-[55px]
+      "
     >
       <Breadcrumbs :breadcrumbs="breadcrumbs" color="#A3A3A3" />
 
       <div
-        class="container mx-auto mt-[20px] xl:grid xl:grid-cols-2 xl:gap-x-[115px]"
+        class="
+          container
+          mx-auto
+          mt-[20px]
+          xl:grid xl:grid-cols-2
+          xl:gap-x-[115px]
+        "
       >
         <div>
           <h1
-            class="font-serif font-light text-[32px] leading-[32px] md:text-[90px] md:leading-[80px]"
+            class="
+              font-serif font-light
+              text-[32px]
+              leading-[32px]
+              md:text-[90px]
+              md:leading-[80px]
+            "
           >
             About me
           </h1>
           <p
-            class="mt-[10px] font-light leading-[22px] md:mt-[20px] md:text-[24px] md:leading-[30px]"
+            class="
+              mt-[10px]
+              font-light
+              leading-[22px]
+              md:mt-[20px]
+              md:text-[24px]
+              md:leading-[30px]
+            "
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -27,7 +54,14 @@
             consequat.
           </p>
           <p
-            class="mt-[10px] font-light leading-[22px] md:mt-[20px] md:text-[24px] md:leading-[30px]"
+            class="
+              mt-[10px]
+              font-light
+              leading-[22px]
+              md:mt-[20px]
+              md:text-[24px]
+              md:leading-[30px]
+            "
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -59,28 +93,70 @@
     <div class="mt-[53px] md:mt-[120px]">
       <div class="container mx-auto px-[17.5px] md:px-[30px] 2xl:px-[55px]">
         <h2
-          class="font-serif font-light text-[24px] leading-[32px] md:text-[60px] md:leading-[86px]"
+          class="
+            font-serif font-light
+            text-[24px]
+            leading-[32px]
+            md:text-[60px]
+            md:leading-[86px]
+          "
         >
           Worked with
         </h2>
       </div>
       <div class="mt-[10px] w-full overflow-y-hidden">
         <div
-          class="pl-[17.5px] md:pl-[30px] 2xl:pl-[55px] font-serif font-light text-[70px] leading-[70px] text-electric-pink uppercase whitespace-nowrap 2xl:text-[180px] 2xl:leading-[180px]"
+          class="
+            pl-[17.5px]
+            md:pl-[30px]
+            2xl:pl-[55px]
+            font-serif font-light
+            text-[70px]
+            leading-[70px]
+            text-electric-pink
+            uppercase
+            whitespace-nowrap
+            2xl:text-[180px]
+            2xl:leading-[180px]
+          "
         >
           Amsterdam Fashion Week - Hermes - Vogue
         </div>
       </div>
       <div class="w-full overflow-y-hidden">
         <div
-          class="pl-[17.5px] md:pl-[30px] 2xl:pl-[55px] font-serif font-light text-[70px] leading-[70px] text-electric-pink uppercase whitespace-nowrap 2xl:text-[180px] 2xl:leading-[180px]"
+          class="
+            pl-[17.5px]
+            md:pl-[30px]
+            2xl:pl-[55px]
+            font-serif font-light
+            text-[70px]
+            leading-[70px]
+            text-electric-pink
+            uppercase
+            whitespace-nowrap
+            2xl:text-[180px]
+            2xl:leading-[180px]
+          "
         >
           Vogue / Hermes
         </div>
       </div>
       <div class="w-full overflow-y-hidden">
         <div
-          class="pl-[17.5px] md:pl-[30px] 2xl:pl-[55px] font-serif font-light text-[70px] leading-[70px] text-electric-pink uppercase whitespace-nowrap 2xl:text-[180px] 2xl:leading-[180px]"
+          class="
+            pl-[17.5px]
+            md:pl-[30px]
+            2xl:pl-[55px]
+            font-serif font-light
+            text-[70px]
+            leading-[70px]
+            text-electric-pink
+            uppercase
+            whitespace-nowrap
+            2xl:text-[180px]
+            2xl:leading-[180px]
+          "
         >
           HERMES / Lorem / Amsterdam Fashion Week - Hermes - Vogue
         </div>
@@ -90,12 +166,26 @@
     <div class="container mx-auto px-[17.5px] md:px-[30px] 2xl:px-[55px]">
       <div class="mt-[51px] md:mt-[105px]">
         <h2
-          class="font-serif font-light text-[24px] leading-[32px] md:text-[60px] md:leading-[86px]"
+          class="
+            font-serif font-light
+            text-[24px]
+            leading-[32px]
+            md:text-[60px]
+            md:leading-[86px]
+          "
         >
           Proces
         </h2>
         <p
-          class="mt-[7px] font-light leading-[22px] 2xl:mt-[14px] 2xl:max-w-[869px] 2xl:text-[24px] 2xl:leading-[30px]"
+          class="
+            mt-[7px]
+            font-light
+            leading-[22px]
+            2xl:mt-[14px]
+            2xl:max-w-[869px]
+            2xl:text-[24px]
+            2xl:leading-[30px]
+          "
         >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -109,7 +199,9 @@
 
       <lite-youtube
         class="mt-[30px] max-w-[1440px] md:mt-[40px]"
-        style="background-image: url('https://i.ytimg.com/vi/ogfYd705cRs/hqdefault.jpg');"
+        style="
+          background-image: url('/images/beyond-the-lipstick-thumbnail.jpg');
+        "
         videoid="A61dZiKAUpI"
         params="controls=0&modestbranding=1&rel=0&enablejsapi=1"
       />
@@ -118,28 +210,70 @@
     <div class="mt-[53px] md:mt-[120px]">
       <div class="container mx-auto px-[17.5px] md:px-[30px] 2xl:px-[55px]">
         <h2
-          class="font-serif font-light text-[24px] leading-[32px] md:text-[60px] md:leading-[86px]"
+          class="
+            font-serif font-light
+            text-[24px]
+            leading-[32px]
+            md:text-[60px]
+            md:leading-[86px]
+          "
         >
           Awards &#38; Nominations
         </h2>
       </div>
       <div class="mt-[10px] w-full overflow-y-hidden">
         <div
-          class="pl-[17.5px] md:pl-[30px] 2xl:pl-[55px] font-serif font-light text-[70px] leading-[70px] text-apple-green uppercase whitespace-nowrap 2xl:text-[180px] 2xl:leading-[180px]"
+          class="
+            pl-[17.5px]
+            md:pl-[30px]
+            2xl:pl-[55px]
+            font-serif font-light
+            text-[70px]
+            leading-[70px]
+            text-apple-green
+            uppercase
+            whitespace-nowrap
+            2xl:text-[180px]
+            2xl:leading-[180px]
+          "
         >
           new wave - 2019 / amfi young talent -2018
         </div>
       </div>
       <div class="w-full overflow-y-hidden">
         <div
-          class="pl-[17.5px] md:pl-[30px] 2xl:pl-[55px] font-serif font-light text-[70px] leading-[70px] text-apple-green uppercase whitespace-nowrap 2xl:text-[180px] 2xl:leading-[180px]"
+          class="
+            pl-[17.5px]
+            md:pl-[30px]
+            2xl:pl-[55px]
+            font-serif font-light
+            text-[70px]
+            leading-[70px]
+            text-apple-green
+            uppercase
+            whitespace-nowrap
+            2xl:text-[180px]
+            2xl:leading-[180px]
+          "
         >
           amfi young talent -2018
         </div>
       </div>
       <div class="w-full overflow-y-hidden">
         <div
-          class="pl-[17.5px] md:pl-[30px] 2xl:pl-[55px] font-serif font-light text-[70px] leading-[70px] text-apple-green uppercase whitespace-nowrap 2xl:text-[180px] 2xl:leading-[180px]"
+          class="
+            pl-[17.5px]
+            md:pl-[30px]
+            2xl:pl-[55px]
+            font-serif font-light
+            text-[70px]
+            leading-[70px]
+            text-apple-green
+            uppercase
+            whitespace-nowrap
+            2xl:text-[180px]
+            2xl:leading-[180px]
+          "
         >
           Dutch fashion award
         </div>
@@ -156,8 +290,8 @@ export default {
     return {
       breadcrumbs: [
         { name: "Home", link: "/" },
-        { name: "About me", link: "/about" }
-      ]
+        { name: "About me", link: "/about" },
+      ],
     };
   },
   head: {
@@ -166,9 +300,9 @@ export default {
       {
         hid: "description",
         name: "description",
-        content: ""
-      }
-    ]
-  }
+        content: "",
+      },
+    ],
+  },
 };
 </script>
