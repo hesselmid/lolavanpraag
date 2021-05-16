35722ab5f11a9b531df002d8366ffc622a37433f
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-05-16 17:05:38 +0200

    wrgkg4go4

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,10 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "Lola van Praag - Menswear, Textile Design | lolavanpraag.com",
     htmlAttrs: {
-      lang: "en",
+      lang: "en"
     },
     meta: [
       { charset: "utf-8" },
@@ -15,33 +13,42 @@ export default {
         hid: "description",
         name: "description",
         content:
-          "Lola van Praag is a Menswear Fashion Designer and Textile Artist based in the Netherlands.",
-      },
+          "Lola van Praag is a Menswear Fashion Designer and Textile Artist based in the Netherlands."
+      }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.svg" },
-      { rel: "stylesheet", href: "https://use.typekit.net/hrx0dfu.css" },
-    ],
+      {
+        rel: "preconnect",
+        href: "https://use.typekit.net",
+        crossorigin: "anonymous"
+      },
+      {
+        rel: "preload",
+        as: "style",
+        href: "https://use.typekit.net/hrx0dfu.css"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://use.typekit.net/hrx0dfu.css",
+        media: "print",
+        onload: "this.media='all'"
+      }
+    ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["node_modules/lite-youtube-embed/src/lite-yt-embed.css"],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     "~/plugins/youtube.client.js",
-    "~/plugins/vue-observe-visibility.client.js",
+    "~/plugins/vue-observe-visibility.client.js"
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: ["@nuxtjs/tailwindcss"],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: ["@nuxt/content"],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {}
 };
