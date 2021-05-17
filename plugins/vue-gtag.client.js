e3bdb743f0a9594cb7240f19919f243e9f283452
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-05-17 14:45:58 +0200

    wrgigiugo4

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,16 +1,26 @@
 import Vue from "vue";
 import VueGtag from "vue-gtag";
 
-export default ({ app }) => {
-  const getGDPR = localStorage.getItem("GDPR:accepted");
+// export default ({ app }) => {
+//   const getGDPR = localStorage.getItem("GDPR:accepted");
 
+//   Vue.use(
+//     VueGtag,
+//     {
+//       config: { id: "UA-102074403-1" },
+//       bootstrap: getGDPR === "true",
+//       enabled: getGDPR === "true",
+//       pageTrackerScreenviewEnabled: true
+//     },
+//     app.router
+//   );
+// };
+
+export default ({ app }) => {
   Vue.use(
     VueGtag,
     {
-      config: { id: "UA-102074403-1" },
-      bootstrap: getGDPR === "true",
-      enabled: getGDPR === "true",
-      pageTrackerScreenviewEnabled: true
+      config: { id: "UA-102074403-1" }
     },
     app.router
   );
